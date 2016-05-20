7ab1b8c84726a055d0215641e31a8b396c6e1655
Ethan Sutin <ethan@sutin.net>
2016-05-20 14:48:18 -0700

    Initial Commit

---
+++ daycationTests/daycationTests.swift
@@ -0,0 +1,36 @@
+//
+//  daycationTests.swift
+//  daycationTests
+//
+//  Created by Ethan on 5/20/16.
+//  Copyright © 2016 Trailhead Labs. All rights reserved.
+//
+
+import XCTest
+@testable import daycation
+
+class daycationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
